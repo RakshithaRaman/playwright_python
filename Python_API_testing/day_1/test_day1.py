a37faee1b86d07cd1ccb9801dafd5167a14cfadb
RakshithaRaman <23rakshitharaman@gmail.com>
2026-03-13 15:02:07 +0530

    commit

---
+++ Python_API_testing/day_1/test_day1.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.mark.dependency()
+@pytest.mark.order(1)
+def test_open_app():
+    print("app opened")
+    assert True
+
+@pytest.mark.dependency(depends=["test_login"])
+@pytest.mark.order(3)
+def test_dashboard():
+    print("dashboard opened")
+
+@pytest.mark.dependency(depends=["test_open_app"])
+@pytest.mark.order(2)
+def test_login():
+    print("login successful")
